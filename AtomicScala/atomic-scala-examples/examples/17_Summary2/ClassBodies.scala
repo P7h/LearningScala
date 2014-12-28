1237d520f752e7fa358a519b239666b2c8a7558c
P7h <prashanth.babu@gmail.com>
2014-12-28 13:46:10 +0000

    Added 25 atoms of Atomic Scala Examples.

---
+++ AtomicScala/atomic-scala-examples/examples/17_Summary2/ClassBodies.scala
@@ -0,0 +1,15 @@
+  // ClassBodies.scala
+
+class NoBody
+val nb = new NoBody
+
+class SomeBody {
+  val name = "Janet Doe"
+  println(name + " is SomeBody")
+}
+val sb = new SomeBody
+
+class EveryBody {
+  val all = Vector(new SomeBody, new SomeBody, new SomeBody)
+}
+val eb = new EveryBody
