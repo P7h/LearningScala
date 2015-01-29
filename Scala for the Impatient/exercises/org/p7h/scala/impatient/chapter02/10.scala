039a6166bda473ab7b1a6c276d16ec57f74dbb46
P7h <prashanth.babu@gmail.com>
2015-01-29 22:47:08 +0000

    Chap2: Exercise 10 of Scala for the Impatient.
    Completes Chapter 2.

--- Scala for the Impatient/exercises/org/p7h/scala/impatient/chapter02/10.scala
+++ Scala for the Impatient/exercises/org/p7h/scala/impatient/chapter02/10.scala
@@ -2,7 +2,7 @@
 
 10. Write a function that computes x<sup>n</sup>, where n is an integer. Use the following recursive definition:
     - x<sup>n</sup> = y<sup>2</sup> if n is even and positive, where y = x <sup>n / 2</sup>.
-    - x<sup>n</sup> = x x<sup>n – 1</sup> if n is odd and positive.
+    - x<sup>n</sup> = x * x<sup>n – 1</sup> if n is odd and positive.
     - x<sup>0</sup> = 1.
     - x<sup>n</sup> = 1 / x<sup>–n</sup> if n is negative.
 
@@ -10,3 +10,11 @@ Don’t use a  return statement.
 
 */
 
+def powerOfXToN(x: BigInt, n: Int) : BigInt = {
+    if(n > 0 && n % 2 == 0) powerOfXToN(x, n / 2) * powerOfXToN(x, n / 2)
+    else if(n > 0) x * powerOfXToN(x, n - 1)
+    else if(n == 0) 1
+    else 1 / powerOfXToN(x, -n)
+}
+
+powerOfXToN(2, 100)
--- Scala for the Impatient/exercises/org/p7h/scala/impatient/chapter02/Exercises02.md
+++ Scala for the Impatient/exercises/org/p7h/scala/impatient/chapter02/Exercises02.md
@@ -11,7 +11,7 @@
 9. Make the function of the preceding exercise a recursive function.
 10. Write a function that computes x<sup>n</sup>, where n is an integer. Use the following recursive definition:
 	- x<sup>n</sup> = y<sup>2</sup> if n is even and positive, where y = x <sup>n / 2</sup>.
-	- x<sup>n</sup> = x x<sup>n – 1</sup> if n is odd and positive.
+	- x<sup>n</sup> = x * x<sup>n – 1</sup> if n is odd and positive.
 	- x<sup>0</sup> = 1.
 	- x<sup>n</sup> = 1 / x<sup>–n</sup> if n is negative.
 
